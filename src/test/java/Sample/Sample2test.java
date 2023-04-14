e4ef6e804472a79d5984b66ef91a30390bcdeb68
Bangaru Raju Gelli <gbrazu@gmail.com>
2023-04-14 11:06:29 +0530

    Sample2

---
+++ src/test/java/Sample/Sample2test.java
@@ -0,0 +1,17 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Sample2test 
+
+{
+	@Test
+	public void add()
+	{
+		int a =10;
+		int b = 20;
+		int c =a+b;
+		System.out.println(c);
+	}
+
+}
